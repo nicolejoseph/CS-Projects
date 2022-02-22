e49a887d647d51faa9fef930b65325dd35143148
nicolejoseph <55464125+nicolejoseph@users.noreply.github.com>
2022-02-22 13:36:06 -0500

    Delete OS directory

--- OS/ShellGame/spinlock.h
+++
@@ -1,5 +0,0 @@
-#ifndef __SPINLOCK_H
-void spin_lock(volatile char *lock);
-void spin_unlock(volatile char *lock);
-#define __SPINLOCK_H
-#endif
--- OS/ShellGame/tas.h
+++
@@ -1,4 +0,0 @@
-#ifndef __TAS_H
-int tas(volatile char *lock);
-#define __TAS_H
-#endif
